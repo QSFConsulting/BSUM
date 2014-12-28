fa877f85088ecd845762654171eafbc86e323bef
Oskari Vinko <oskari.vinko@gmail.com>
2014-12-28 12:16:30 +0200

    Prettifying

--- cms/views.py
+++ cms/views.py
@@ -6,7 +6,5 @@ import os
 def get_news(request):
     news_objects = News.objects.all().order_by("-updated")
     print os.path.join(
-    os.path.dirname(__file__), 'static'
-).replace('\\', '/')
+        os.path.dirname(__file__), 'static').replace('\\', '/')
     return render(request, 'index2.html', {'news_objects': news_objects})
-
