f1093e9fef671bc5789d26862b89896b369f1184
Oskari Vinko <oskari.vinko@gmail.com>
2015-01-04 11:40:37 +0200

    URL fix

--- cms/urls.py
+++ cms/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # url(r'^$', 'cms.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^', 'cms.views.get_index'),
+    url(r'^$', 'cms.views.get_index'),
     url(r'^news/', 'cms.views.get_news'),
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^static/admin/', include(admin.site.urls)),
