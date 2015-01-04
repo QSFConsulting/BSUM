1d26a84c2f9ddeb97bfb4965404bdc921af61330
Oskari Vinko <oskari.vinko@gmail.com>
2015-01-04 10:59:12 +0200

    main page view and news view separated

--- cms/views.py
+++ cms/views.py
@@ -3,6 +3,13 @@ from models import News
 import os
 
 
+def get_index(request):
+    news_objects = News.objects.all().order_by("-updated")
+    print os.path.join(
+        os.path.dirname(__file__), 'static').replace('\\', '/')
+    return render(request, 'index.html', {'news_objects': news_objects})
+
+
 def get_news(request):
     news_objects = News.objects.all().order_by("-updated")
     print os.path.join(
