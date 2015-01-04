6ed2e17feb27e3f86eff7c56eee42bfcc310eb0c
Oskari Vinko <oskari.vinko@gmail.com>
2015-01-04 11:41:34 +0200

    News cut

--- cms/views.py
+++ cms/views.py
@@ -4,14 +4,17 @@ import os
 
 
 def get_index(request):
-    news_objects = News.objects.all().order_by("-updated")
-    print os.path.join(
-        os.path.dirname(__file__), 'static').replace('\\', '/')
+    news_objects = News.objects.all().order_by("-updated")[:2]
+    for new in news_objects:
+        new.cut = False
+        if len(new.content):
+            tmp_content = new.content[:360]
+            tmp_content = " ".join(tmp_content.split(" ")[:-1]) + "..."
+            new.content = tmp_content
+            new.cut = True
     return render(request, 'index.html', {'news_objects': news_objects})
 
 
 def get_news(request):
     news_objects = News.objects.all().order_by("-updated")
-    print os.path.join(
-        os.path.dirname(__file__), 'static').replace('\\', '/')
-    return render(request, 'index.html', {'news_objects': news_objects})
+    return render(request, 'news.html', {'news_objects': news_objects})
