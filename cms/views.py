3a434c10f431b611e580c82affe5170c2d6983ae
Oskari Vinko <oskari.vinko@gmail.com>
2015-01-04 10:58:56 +0200

    Reorganizing index.html into templates

--- cms/views.py
+++ cms/views.py
@@ -7,4 +7,4 @@ def get_news(request):
     news_objects = News.objects.all().order_by("-updated")
     print os.path.join(
         os.path.dirname(__file__), 'static').replace('\\', '/')
-    return render(request, 'index2.html', {'news_objects': news_objects})
+    return render(request, 'index.html', {'news_objects': news_objects})
