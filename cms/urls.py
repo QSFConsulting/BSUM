e92964196ff513ce892faf7da2acaa9a9297dec9
Oskari Vinko <oskari.vinko@gmail.com>
2015-01-04 10:42:36 +0200

    Fix

--- cms/urls.py
+++ cms/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     # url(r'^$', 'cms.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
