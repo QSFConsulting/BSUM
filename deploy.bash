477c8a4bfb3958ffe8eb79db51afd8f02325e4e0
Oskari Vinko <oskari.vinko@gmail.com>
2015-01-09 16:15:09 +0200

    New deploy script

--- deploy.bash
+++ deploy.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Did you change manage.py settings into production settings?"
-scp ~/BSUM/manage.py root@188.166.37.173:/home/django/django_project/manage.py
-scp -r ~/BSUM/cms root@188.166.37.173:/home/django/django_project/
+#scp ~/BSUM/manage.py root@188.166.37.173:/home/django/django_project/manage.py
+#scp -r ~/BSUM/cms root@188.166.37.173:/home/django/django_project/
+
+
+scp ~/BSUM/manage.py root@188.166.28.185:/home/django/django_project/manage.py
+scp -r ~/BSUM/cms root@188.166.28.185:/home/django/django_project/
