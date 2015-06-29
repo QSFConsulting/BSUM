f204f4a2dd4325a4a65d05806e5c3e2e1d97d7d2
Vamiyn <vamiynr@gmail.com>
2015-06-29 15:22:38 +0300

    references switch

--- deploy.bash
+++ deploy.bash
@@ -3,6 +3,6 @@ echo "Did you change manage.py settings into production settings?"
 #scp ~/BSUM/manage.py root@188.166.37.173:/home/django/django_project/manage.py
 #scp -r ~/BSUM/cms root@188.166.37.173:/home/django/django_project/
 
-
+#check that the path matches!
 scp ~/BSUM/manage.py root@188.166.28.185:/home/django/django_project/manage.py
 scp -r ~/BSUM/cms root@188.166.28.185:/home/django/django_project/
